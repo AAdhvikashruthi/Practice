54d813cf757af847c4ca99fd66c862b1dc466610
AAdhvikashruthi <shruthigm14@gmail.com>
2023-11-30 15:35:15 +0530

    job is done

---
+++ src/test/java/com/Sspot/Practice1Test.java
@@ -0,0 +1,18 @@
+package com.Sspot;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	@Test
+	public void createTest()
+	{
+		System.out.println("test is created");
+	}
+	
+	@Test
+	public void modiftTest()
+	{
+		System.out.println("test2 is created");
+	}
+
+}
---
+++ src/test/java/com/Sspot/Practice2Test.java
@@ -0,0 +1,17 @@
+package com.Sspot;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+	@Test
+	public void productTest()
+	{
+		System.out.println("product is created");
+	}
+	@Test
+	public void product2Test()
+	{
+		System.out.println("Product 2 is created");
+	}
+
+}
